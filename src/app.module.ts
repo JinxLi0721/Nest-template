e2346f4341244834f142ddb24236e029d9aef390
Jin <bestadeveloper@BestadeMacBook-Air.local>
2025-08-27 15:02:03 +0800

    feat(employee): use Prisma database to create module & fix prisma client bug

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { DatabaseModule } from './database/database.module';
+import { EmployeeModule } from './employee/employee.module';
 
 @Module({
-  imports: [UserModule, DatabaseModule],
+  imports: [UserModule, DatabaseModule, EmployeeModule],
   controllers: [AppController],
   providers: [AppService],
 })
